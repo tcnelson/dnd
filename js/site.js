559c9f4068845395f65d94d48cefc3afe9a21c78
tcnelson <Trax1315@gmail.com>
2015-08-28 16:10:02 -0700

    still working on calculating skill checks

--- js/site.js
+++ js/site.js
@@ -2,14 +2,38 @@ $(function () {
   $('[data-toggle="popover"]').popover();
 
   $('.modifier').change(function() {
-  	var value = $(this).val();
-  	var modifier = $(this).data('modifier');
+  	var target = $(this).data('target');
 
-  	var calculated = Math.floor((value - 10) / 2);
-
-  	var attributes = $('[data-modifier="' + modifier + '"]');  //'[data-modifier="strength"]'
-  	for (var i = 1; i < attributes.length; i++){
-		$(attributes[i]).val(calculated);
-  	}
+  	var attributes = $(target);  
+  	for (var i = 0; i < attributes.length; i++){
+		updateAttribute(attributes[i]);
+  	} 
   });
+
+  $('.proficiency').change(function() {
+  	var target = $(this).data('target');
+
+  	updateAttribute(target);
+  	} 
+  });
+
+  function updateAttribute (el) {
+  	var proficiencyBonus = $('#proficiency-bonus').val();
+  	var modifier = $(el).data('modifier');
+  	var value = $(modifier).val();	
+  	
+  	var proficiencySelector = $(el).data('proficiency');        
+    var proficient = $(proficiencySelector).attr('checked');
+
+  	if (proficient) {
+  		var calculated = Math.floor((value - 10) / 2) + proficiencyBonus;
+  	}
+
+  	else {
+  		var calculated = Math.floor((value - 10) / 2);
+  	}
+
+  	$(el).val(calculated);
+  	
+  }
 })
